f5b4144ecc744acc587bb2e815c77f6de3c51716
Angel Rodríguez <angelrodriguez@MBP-de-Angel.localdomain>
2020-01-22 12:12:11 -0600

    Understanding - Props, children property, components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">Welcome to React</h1>
+        <p>Description working</p>
+        <Person name="Angeel" age="27" />
+        <Person name="Alonzo" age="29">
+          My hobbies: Slepping{" "}
+        </Person>
       </div>
     );
   }
